fb5b6242ec2f26d3d8203364a7f69bf6ca0062a6
Muhammad Awais <awaisahmedindher66@gmail.com>
2026-07-04 19:11:25 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default async function Home() {
         <Hero slides={heroSlides} />
         <Countdown />
         <CollectionTiles />
-        <BrandStory />
+        
         <ProductSections products={products} />
         <Reviews />
         <ContactForm />
